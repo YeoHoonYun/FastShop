45da2a867e209e4654852937492087ef56b593e5
DESKTOP-1NBEG5A\user <cjswo9207@naver.com>
2018-09-02 14:47:21 +0900

    no message

--- src/main/java/com/fastcompus/FastBlog/HomeController.java
+++ src/main/java/com/fastcompus/FastBlog/HomeController.java
@@ -11,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.fastcompus.FastBlog.bo.UserBO;
 import com.fastcompus.FastBlog.dao.UserDAO;
 import com.fastcompus.FastBlog.vo.UserVO;
 
